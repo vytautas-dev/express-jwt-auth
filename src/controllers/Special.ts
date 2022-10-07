e908b5eba7e74b503cba728b9da1533efa18c511
Wojciech Szczygielski <wojciech.szczygielski91@gmail.com>
2022-10-07 14:04:22 +0200

    uninstall unnecessary packages

--- src/controllers/Special.ts
+++ src/controllers/Special.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
 const special = (req: Request, res: Response) => {
-  console.log(req);
+  console.log(req.body.email);
   return res.json({ msg: `Hey ${req.body.email}!` });
 };
 
