6c2db5fc100e960b4e5284c8cea18383e4f9ea26
Wojciech Szczygielski <wojciech.szczygielski91@gmail.com>
2022-10-09 18:44:00 +0200

    fix response message in special(protected) route

--- src/controllers/Special.ts
+++ src/controllers/Special.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express";
+import jwt_decode from "jwt-decode";
 
 const special = (req: Request, res: Response) => {
-  console.log(req.body.email);
-  return res.json({ msg: `Hey ${req.body.email}!` });
+  const token = req.headers.authorization
+    ? req.headers.authorization.split(" ")[1]
+    : "";
+  const decoded: { id: string } = jwt_decode(token);
+
+  return res.json({ msg: `Hey user id: ${decoded.id}!` });
 };
 
 export default { special };
